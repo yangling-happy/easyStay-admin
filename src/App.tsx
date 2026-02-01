4165a69680e431c66b051a3410afdd9b18b6923d
Amber Yang <yamber0302@qq.com>
2026-02-01 16:28:10 +0800

    feat: HotelEdit 模块接入到 App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { MainLayout } from "./layouts/MainLayout";
 import { ConfigProvider } from "antd";
 import zhCN from "antd/locale/zh_CN";
+import { MainLayout } from "./layouts/MainLayout";
 import HotelAudit from "./pages/HotelAudit";
-
-// 临时占位组件
+import HotelEdit from "./pages/HotelEdit"; 
+// 临时占位组件 
 const HotelList = () => <div>酒店管理列表页</div>;
-const HotelEdit = () => <div>酒店录入/编辑页</div>;
 
 function App() {
   return (
     <ConfigProvider locale={zhCN}>
       <BrowserRouter>
         <Routes>
+          {/* 主布局路由 */}
           <Route path="/" element={<MainLayout />}>
-            {/* 默认跳转到列表页 */}
+            {/* 首页重定向到列表 */}
             <Route index element={<Navigate to="/hotels" replace />} />
 
-            {/* 酒店管理主路由 */}
-            <Route path="hotels" element={<HotelList />} />
-            <Route path="hotels/new" element={<HotelEdit />} />
-            <Route path="hotels/edit/:id" element={<HotelEdit />} />
-            <Route path="hotels/audit" element={<HotelAudit />} />
-
-            <Route path="hotel-edit" element={<HotelEdit />} />
+            {/* 酒店管理相关路由 */}
+            <Route path="hotels">
+              <Route index element={<HotelList />} />
+              <Route path="new" element={<HotelEdit />} />
+              <Route path="edit/:id" element={<HotelEdit />} />
+              <Route path="audit" element={<HotelAudit />} />
+            </Route>
 
             {/* 404 页面 */}
             <Route path="*" element={<div>404 Not Found</div>} />
